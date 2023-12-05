790196f059fa9a67b52c0039ab11022c6e078b96
Syed Shoeb Uddin <ushoeb25@gmail.com>
2023-12-05 23:23:24 +0530

    Spiltter Deployed

---
+++ README.md
@@ -0,0 +1,5 @@
+# ---Bill---Spliiter---
+
+Used basic Javascript and a window interval for resetting the values and webpage within 2.5 seconds.
+
+Vanilla CSS used for the looks of the page.
---
+++ script.js
@@ -0,0 +1,27 @@
+function clicked(){
+    let a= document.getElementById('amount');
+    let p= document.getElementById('persons');
+
+    const amount= Number(a.value);
+    const persons=Number(p.value);
+
+
+    ans=(amount/persons).toFixed(2);
+
+    let result=document.getElementById('screen');
+    if(amount==0 || persons==0){
+        result.innerHTML="Give Inputs.";
+    }
+    else if(amount<0 || persons<0){
+        result.innerHTML="Please enter valid Positive values";
+    }
+    else{
+        result.style.fontSize='48px'
+        result.innerHTML=(ans +" each");
+    }
+    window.setInterval(() => {
+        window.location.reload();
+        a.value="";
+        p.value="";
+    }, 2500);
+}
